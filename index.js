05b3d2b2fb25794bf0cb855adea109a1bc667e97
aboura <bouchrabichou638@gmail.com>
2023-07-28 10:24:49 +0200

    completed js lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(name) {
+    const msg =[];
+for (let i=0; i < name.length; i++ ) {
+     msg.push (`Thank you, ${name [i]}, for the wonderful surprise gift!`);
+}
+return msg;
+}
+let names=["Guadalupe", "Ollie", "Aki"]; 
+ console.log(writeCards(names));
+  
+ function countDown(number) {
+    let i = number; 
+    while (i >= 0) { 
+      console.log(i);
+      i--;
+    }
+  }
+  
+  countDown(10);
